dde6906da3a6bbdb829df9ad6a486a1e412b722e
JCBEST <jcbest@192-168-1-106.tpgi.com.au>
2020-08-17 22:10:12 +1000

    new edits

--- src/Components/IntroPage/IntroPage.js
+++ src/Components/IntroPage/IntroPage.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import Chip from '@material-ui/core/Chip';
 import Typography from '@material-ui/core/Typography';
 import {makeStyles} from "@material-ui/core/styles";
 
@@ -11,7 +15,9 @@ const useStyles = makeStyles((theme) =>({
   button: {
     textTransform: "none",
     width: 170,
-    margin: theme.spacing(1),
+    marginTop: 20,
+    margin: theme.spacing(5),
+    marginBottom: 20,
   },
   root: {
     justifyContent: 'center',
@@ -21,6 +27,30 @@ const useStyles = makeStyles((theme) =>({
     width: theme.spacing(40),
     height: theme.spacing(40),
   },
+  bio: {
+    minWidth: 550,
+    maxWidth: 550,
+    justifyContent: 'center',
+  },
+  description: {
+    minWidth: 550,
+    maxWidth: 550,
+  },
+  centertext: {
+    textAlign: 'center',
+  },
+  chips: {
+    marginTop: 20,
+    margin: theme.spacing(3),
+    marginBottom: 20,
+    justifyContent: 'center',
+    minWidth: 130,
+    maxWidth: 130,
+  },
+  chipssection: {
+    justifyContent: 'space-evenly',
+  }
+
 }));
 
 export default function IntroPage() {
@@ -33,10 +63,40 @@ export default function IntroPage() {
       </CardActions>
 
       <CardActions classes={{root: button_classes.root}}>
-        <Typography variant="h4" gutterBottom>
+        <Typography variant="h4">
           Johnson Chau
         </Typography>
       </CardActions>
+
+      <CardActions classes={{root: button_classes.root}}>
+        <Card className={button_classes.bio}>
+          <CardContent>
+            <Typography variant="body1" component="p" align="center">
+              A 2nd year Business Analytics and Computer Science student at University of Sydney, Active Programmer <span role="img" aria-label="computer">🖥</span> and Problem Solver <span role="img" aria-label="puzzle">🧩</span>
+            </Typography>
+          </CardContent>
+        </Card>
+      </CardActions>
+
+      <CardActions className={button_classes.chipssection}>
+        <Card className={button_classes.description}>
+          <CardHeader
+            title="Personality" className={button_classes.centertext}
+          />
+          <Chip className={button_classes.chips} label="Team Player" clickable/>
+          <Chip className={button_classes.chips} label="Communicator" clickable/>
+          <Chip className={button_classes.chips} label="Curious" clickable/>
+        </Card>
+        <Card className={button_classes.description}>
+          <CardHeader
+            title="Skills" className={button_classes.centertext}
+          />
+          <Chip className={button_classes.chips} label="Python" clickable/>
+          <Chip className={button_classes.chips} label="Java" clickable/>
+          <Chip className={button_classes.chips} label="C" clickable/>
+        </Card>
+      </CardActions>
+
       <CardActions classes={{root: button_classes.root}}>
         <Button className={button_classes.button} variant="outlined" fullWidth="false" href="Johnson_Chau_CV.pdf" target="_blank" >Resume</Button>
         <Button className={button_classes.button} variant="outlined" href="Johnson_Chau_Academic_Transcript.pdf" target="_blank">Academic Transcript</Button>
